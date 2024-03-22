4fafae1a43e9e5502f7a4ad3ec7468d14051dcd1
CS31IvanK <kaftanatii2021ks12@student.karazin.ua>
2024-03-22 21:59:14 +0100

    make checking db existanse + get method working

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,31 @@
-from flask import Flask, abort
+from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import datetime
-from sqlalchemy import create_engine
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 CORS(app)
 
+engine = create_engine('mysql+pymysql://root:root@db')
+
+with engine.connect() as connection:
+    connection.execute(text('CREATE DATABASE IF NOT EXISTS mydatabase'))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db:3306/mydatabase'
 db = SQLAlchemy(app)
-class Table(db.Model):
-    __tablename__ = 'example'
+class MyTable(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128))
-
-@app.route('/data')
-def get_data():
-    all_data = Table.query.all()
-    return jsonify([{'id': item.id, 'name': item.name} for item in all_data])
-
+    def to_json(self):
+       return {
+            'id': self.id,
+        }
+    
+@app.route('/table', methods=['GET'])
+def get_table():
+    table_data = MyTable.query.all()
+    return jsonify([row.to_json() for row in table_data])
 
 @app.route("/")
 def run():
@@ -29,4 +35,6 @@ def run():
     }
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=6001, host="0.0.0.0")
