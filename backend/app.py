a5ec9c26e3bde5923981a1a2b20ea53208d206bf
CS31IvanK <kaftanatii2021ks12@student.karazin.ua>
2024-03-29 18:34:12 +0100

    Add all requied funcs (working)

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, text
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +16,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-engine = create_engine('mysql+pymysql://root:root@db')
+engine = create_engine('mysql+pymysql://root:root@db', pool_pre_ping=True)
 with engine.connect() as connection:
     connection.execute(text('CREATE DATABASE IF NOT EXISTS mydatabase'))
     connection.commit
@@ -23,7 +24,13 @@ with engine.connect() as connection:
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(32), unique=True)
-    password = db.Column(db.String(64))
+    password_hash = db.Column(db.String(64))
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def to_json(self):
        return {
             'id': self.id,
@@ -40,11 +47,10 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        if user:
-            if user.password == password:
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html')
+        if user and check_password_hash(password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return jsonify({'response': False})
 
 @app.route('/logout')
 @login_required
@@ -52,11 +58,26 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('login.html')
+#@app.route('/dashboard')
+#@login_required
+#def dashboard():
+#    return render_template('frontend\pages\login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return jsonify({'message' : 'User already exists'}), 400
+        else:
+            new_user = User(username=username)
+            new_user.set_password(password) 
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('frontend\pages\login.html'))
+    return jsonify({'response': False})
 
 @app.route("/")
 def run():
