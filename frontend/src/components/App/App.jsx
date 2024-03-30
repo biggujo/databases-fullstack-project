613b739bb47ed7430b94037839f19c5928d3dad9
Артём <dokegiti@gmail.com>
2024-03-30 23:26:02 +0200

    Make sign up form (other route)

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,17 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import { AboutPage, HomePage } from '../../pages/index.js';
 import Layout from '../Layout/index.js';
 import SignInForm from '../../pages/SignInForm.jsx';
-import TasksPage from '../../pages/TasksPage.jsx'; // Импортируйте компонент SignInForm
+import SignUpForm from "../../pages/SignUpForm.jsx";
+import TasksPage from '../../pages/TasksPage.jsx';
 
 function App() {
-  return (<Routes>
-      <Route path={'/'} element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path={'about'} element={<AboutPage />} />
-        <Route path={'tasks'} element={<TasksPage />} />
-      </Route>
-      <Route path={'/signin'} element={
-        <SignInForm />} /> {/* Добавьте маршрут для вашей новой страницы */}
+    return (<Routes>
+        <Route path={'/'} element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path={'about'} element={<AboutPage />} />
+            <Route path={'tasks'} element={<TasksPage />} />
+        </Route>
+        <Route path={'/signin'} element={<SignInForm />} />
+        <Route path={'/signup'} element={<SignUpForm />} />
     </Routes>);
 }
 
