9bf92a645a2693af71a650b470319b6f19c4d85a
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-07 18:19:01 +0300

    Move DB connection to the 'database' folder

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,14 @@
-from datetime import datetime
-
-from helpers.main import app, db
-from models import user_model
-from flask import request
 from flask_cors import CORS
-from flask_login import LoginManager
 from routes import user_route
-
-from sqlalchemy import create_engine, text
+from helpers.main import app, db
+from database.config import init_db_connection
 
 CORS(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+init_db_connection()
 
-engine = create_engine('mysql+pymysql://root:root@db', pool_pre_ping=True)
-with engine.connect() as connection:
-    connection.execute(text('CREATE DATABASE IF NOT EXISTS mydatabase'))
-    connection.commit
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
+# Routers
 app.register_blueprint(user_route.blueprint, url_prefix='/api/users')
-
-
-@app.route("/")
-def run():
-    return {
-        "msg": "Hello, World!",
-        "time": datetime.now().timestamp()
-    }
-
 
 if __name__ == "__main__":
     with app.app_context():
---
+++ backend/database/config.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, text
+
+
+def init_db_connection():
+    engine = create_engine('mysql+pymysql://root:root@db', pool_pre_ping=True)
+    with engine.connect() as connection:
+        connection.execute(text('CREATE DATABASE IF NOT EXISTS development'))
+        connection.commit
