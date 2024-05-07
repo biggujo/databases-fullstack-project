201121dfa3d629fba30a68c96b0c3eeaccfd9b22
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-07 18:11:48 +0300

    Add basic MVC scaffold

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,15 @@
-from flask import Flask, jsonify, session
-from flask_cors import CORS
 from datetime import datetime
-from flask import Flask, jsonify, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, text
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
+from helpers.main import app, db
+from models import user_model
+from flask import request
+from flask_cors import CORS
+from flask_login import LoginManager
+from routes import user_route
+
+from sqlalchemy import create_engine, text
 
 CORS(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db:3306/mydatabase'
-app.secret_key = "8H0rVLxb0vR93RP04AnFFLHD"
-
-db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -25,82 +20,12 @@ with engine.connect() as connection:
     connection.commit
 
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), unique=True)
-    password_hash = db.Column(db.String(256))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def to_json(self):
-        return {
-            'id': self.id,
-            'username': self.username
-        }
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    body = request.json
-
-    username = body['username']
-    password = body['password']
-
-    user = User.query.filter_by(username=username).first()
-
-    auth_error = {
-        "message": "Invalid login or password",
-    }, 400
-
-    if user is None:
-        return auth_error
-
-    if not user.check_password(password):
-        return auth_error
-
-    session['logged_in'] = True
-    return user.to_json(), 201
-
-
-@app.route('/logout', methods=['DELETE'])
-def logout():
-    if 'logged_in' not in session:
-        return {
-            "message": "Already logged out"
-        }, 400
-
-    session.pop('logged_in')
-    return {}, 204
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    body = request.json
-
-    username = body['username']
-    password = body['password']
-
-    user = User.query.filter_by(username=username).first()
-
-    if user is not None:
-        return {'message': 'User already exists'}, 400
-
-    new_user = User(username=username)
-    new_user.set_password(password)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return new_user.to_json()
+app.register_blueprint(user_route.blueprint, url_prefix='/api/users')
 
 
 @app.route("/")
