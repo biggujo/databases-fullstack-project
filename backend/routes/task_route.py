20dde98be8f83dcb856bd94b21176fcd8ce0f077
CS31IvanK <kaftanatii2021ks12@student.karazin.ua>
2024-05-09 21:32:44 +0200

    Add restfull routes

--- backend/routes/task_route.py
+++ backend/routes/task_route.py
@@ -4,7 +4,7 @@ from controllers import task_controller
 blueprint = Blueprint('task_route', __name__)
 
 blueprint.route('/', methods=['GET'])(task_controller.index)
-blueprint.route('/create', methods=['POST'])(task_controller.create)
+blueprint.route('/', methods=['POST'])(task_controller.create)
 blueprint.route('/<int:id>', methods=['GET'])(task_controller.get)
-blueprint.route('/update/<int:id>', methods=['PUT'])(task_controller.update)
-blueprint.route('/delete/<int:id>', methods=['DELETE'])(task_controller.delete)
+blueprint.route('/<int:id>', methods=['PUT'])(task_controller.update)
+blueprint.route('/<int:id>', methods=['DELETE'])(task_controller.delete)
