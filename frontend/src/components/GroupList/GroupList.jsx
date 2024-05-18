e78bc9b2230e28730e16b21158f908b07364c973
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-18 15:26:35 +0300

    Add fuzzy search

--- frontend/src/components/GroupList/GroupList.jsx
+++ frontend/src/components/GroupList/GroupList.jsx
@@ -1,26 +1,40 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { List, ListItem, Text } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectGroups } from '../../redux/groups/selectors.js';
 import { GroupsOperations } from '../../redux/groups/operations.js';
 import GroupItem from '../GroupItem/index.js';
 import { selectGroupsNameFilter } from '../../redux/filters/selectors.js';
+import Fuse from 'fuse.js';
 
 export default function GroupList() {
   const dispatch = useDispatch();
   const groups = useSelector(selectGroups);
   const nameFilter = useSelector(selectGroupsNameFilter);
+  const fuse = useRef(new Fuse(groups, {
+    keys: [
+      'name',
+    ],
+  }));
 
   useEffect(() => {
     dispatch(GroupsOperations.fetchAllGroups());
   }, []);
 
+  useEffect(() => {
+    fuse.current = new Fuse(groups, {
+      keys: [
+        'name',
+      ],
+    });
+  }, [groups]);
+
   const filteredGroups = useMemo(() => {
-    if (nameFilter === '') {
+    if (nameFilter.trim() === '') {
       return groups;
     }
 
-    return groups.filter(({ name }) => name.toLowerCase().includes(nameFilter.toLowerCase()));
+    return fuse.current.search(nameFilter).map(({ item }) => item);
   }, [
     groups,
     nameFilter,
