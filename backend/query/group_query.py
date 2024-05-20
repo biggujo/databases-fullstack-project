71312f08a283c06f35f70a7a5010b4cbe036ce62
CS31IvanK <144357899+CS31IvanK@users.noreply.github.com>
2024-05-20 21:25:07 +0200

    Add querry (#39)
    * Add group_query
    
    * Fix group_query
    
    * Fix group_query routes
    
    * Add subtasks resources (#42)
    
    * Add subtask resources
    
    * Merge subtasks to tasks
    
    * Revert "Add subtasks resources (#42)" (#43)
    
    This reverts commit 95aab6d6874375251528df8f427bb175d1390161.
    
    * Add subtasks resources (#44)
    
    * Add subtask resources
    
    * Add i18n on frontend with i18next (EN/UA) (#40)
    
    * Add draft for i18n
    
    * initialise i18n
    
    * Add i18n on frontend with i18next (EN/UA)
    
    * Add missing context provider
    
    ---------
    
    Co-authored-by: Dmytro Ivashchenko <gen.style22@gmail.com>
    
    * Merge subtasks to tasks
    
    * Query object for tasks (#41)
    
    * Add draft for i18n
    
    * initialise i18n
    
    * Add i18n on frontend with i18next (EN/UA)
    
    * Implement a query object for tasks
    
    * Fix sort method
    
    * Fix sort by deadline
    
    * Fix filter by deadline
    
    * Fix filter by date range
    
    * Fix rest of filters
    
    * Add Query Object to groups
    
    ---------
    
    Co-authored-by: Артём <dokegiti@gmail.com>
    
    * Fix minor issues
    
    * Fix tasks-subtasks relationship
    
    ---------
    
    Co-authored-by: ElCaramel <161153467+ElCaramel@users.noreply.github.com>
    Co-authored-by: Dmytro Ivashchenko <gen.style22@gmail.com>
    Co-authored-by: Артём <dokegiti@gmail.com>
    
    * Revert "Add subtasks resources (#44)" (#45)
    
    This reverts commit 82173df77db9bf2c990b72a60ee05425236567ce.
    
    * Change group query
    
    ---------
    
    Co-authored-by: ElCaramel <161153467+ElCaramel@users.noreply.github.com>
    Co-authored-by: Dmytro Ivashchenko <gen.style22@gmail.com>
    Co-authored-by: Артём <dokegiti@gmail.com>

---
+++ backend/query/group_query.py
@@ -0,0 +1,49 @@
+from flask import session
+from models.group_model import Group
+from sqlalchemy.sql import func
+
+from models.user_model import User
+
+
+class GroupsQuery:
+    def __init__(self, initial_scope):
+        self.total = None
+        self.scope = initial_scope
+
+    def call(self, parameters):
+        self._sort_by_name(parameters)
+        self._filter_by_members(parameters)
+        self._filter_joined(parameters)
+        return self._paginate(parameters)  # Returns Pagination object
+
+    def _sort_by_name(self, parameters):
+        order = parameters.get('order')
+
+        if order == 'asc':
+            self.scope = self.scope.order_by(func.lower(Group.name).asc())
+        elif order == 'desc':
+            self.scope = self.scope.order_by(func.lower(Group.name).desc())
+
+    def _filter_by_members(self, parameters):
+        n = parameters.get('n')
+
+        if n is not None:
+            self.scope = self.scope.join(Group.users).group_by(Group.id).having(func.count(User.id) > n)
+
+    def _filter_joined(self, parameters):
+        joined = parameters.get('joined')
+        if joined is not None and joined.lower() == 'true':
+            user = User.query.get(session.get('id'))
+            if user is None:
+                return {'message': 'User not found'}, 404
+            self.scope = self.scope.filter(Group.users.contains(user))
+
+    def _paginate(self, parameters):
+        page = parameters.get('page', 1)
+        per_page = parameters.get('per_page', 5)
+
+        if page is None or per_page is None:
+            return
+
+        return self.scope.paginate(page=int(page), per_page=int(per_page), error_out=False, count=True)
+
