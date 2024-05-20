0333385a7293d46f7e1ec78d7571577d9028b1c7
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-20 12:51:33 +0300

    Query object for tasks (#41)
    * Add draft for i18n
    
    * initialise i18n
    
    * Add i18n on frontend with i18next (EN/UA)
    
    * Implement a query object for tasks
    
    * Fix sort method
    
    * Fix sort by deadline
    
    * Fix filter by deadline
    
    * Fix filter by date range
    
    * Fix rest of filters
    
    * Add Query Object to groups
    
    ---------
    
    Co-authored-by: Артём <dokegiti@gmail.com>

--- backend/controllers/task_controller.py
+++ backend/controllers/task_controller.py
@@ -1,20 +1,30 @@
 from datetime import datetime
-
 from models.task_model import Task
 from models.task_meta_model import TaskMeta
 from helpers.main import db
 from schemas.task_schemas import validate_task
 from flask import request, jsonify, session
-
 from decorators.authorize_user import authorize_user
+from query.tasks_query import TasksQuery
+import math
 
 
 def index():
     user_id = session.get("id")
     if user_id is None:
         return {'message': 'Unauthorized'}, 401
-    return jsonify(json_list=[i.serialize for i in
-                              Task.query_user_tasks(user_id).all()])
+
+    parameters = request.args
+    initial_scope = Task.query_user_tasks(user_id)
+
+    query_object = TasksQuery(initial_scope)
+    pagination_scope = query_object.call(parameters)
+
+    return jsonify(json_list=[task.serialize for task in pagination_scope.items],
+                   page=pagination_scope.page,
+                   per_page=pagination_scope.per_page,
+                   totalPages=math.ceil(pagination_scope.total / pagination_scope.per_page),
+                   )
 
 
 @validate_task
@@ -24,15 +34,13 @@ def create():
 
     name = body.get('name')
     description = body.get('description')
-
     deadline = body.get('deadline')
-    new_task = Task(name=name, description=description, deadline=deadline)
 
+    new_task = Task(name=name, description=description, deadline=deadline)
     db.session.add(new_task)
     db.session.commit()
 
     new_task_meta = TaskMeta(task_id=new_task.id, user_id=user_id)
-
     db.session.add(new_task_meta)
     db.session.commit()
 
@@ -45,12 +53,10 @@ def update(id):
     user_id = session.get("id")
 
     task = Task.query_user_tasks(user_id).filter_by(id=id).first()
-
     if task is None:
         return {'message': 'Task not found'}, 404
 
     fields = ['name', 'description', 'isDone', 'deadline']
-
     for field in fields:
         if field in body:
             setattr(task, field, body.get(field))
@@ -65,7 +71,6 @@ def update(id):
 def delete(id):
     user_id = session.get("id")
     task = Task.query_user_tasks(user_id).filter_by(id=id).first()
-
     if task is None:
         return {'message': 'Task not found'}, 404
 
@@ -79,7 +84,6 @@ def delete(id):
 def get(id):
     user_id = session.get("id")
     task = Task.query_user_tasks(user_id).filter_by(id=id).first()
-
     if task is None:
         return {'message': 'Task not found'}, 404
 
